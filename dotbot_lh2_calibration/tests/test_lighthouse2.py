4e046c4bef099bc54ef343658b435c4b96b1dfd1
Alexandre Abadie <alexandre.abadie@inria.fr>
2026-01-14 15:11:05 +0100

    dotbot_lh2_calibration/tests: fix expected values

--- dotbot_lh2_calibration/tests/test_lighthouse2.py
+++ dotbot_lh2_calibration/tests/test_lighthouse2.py
@@ -7,5 +7,5 @@ from dotbot_lh2_calibration.lighthouse2 import calculate_camera_point
 
 def test_camera_points():
     assert calculate_camera_point(49341, 85887, 1) == pytest.approx(
-        (-0.4255775370509014, 0.15468717476270966)
+        (-0.43435315273542, 0.1512338330873567)
     )
